5923d494a322c012f6f91b6a29bc54d1da8f4364
hieu vuong <vuonghieu283@gmail.com>
2022-06-25 10:35:36 +0700

    update bai thi

---
+++ src/main/java/com/example/demo/entity/Employee.java
@@ -0,0 +1,20 @@
+package com.example.demo.entity;
+    import lombok.*;
+import javax.persistence.*;
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @ToString
+    @Builder
+    @Entity
+    @Table(name = "employees")
+    public class Employee {
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)//
+        private int id;
+        private String name;
+        private Double wage;
+    }
+
